45d01a86772f0f50fee6615d255126153bf1ec36
jelizavetal <115002331+jelizavetaleonova@users.noreply.github.com>
2022-10-11 12:41:31 +0000

    repl

--- main.py
+++ main.py
@@ -1,5 +1,128 @@
-sk1=int(input("Ievadiet skaitli: "))
-if sk1>10:
- print("Skaitlis ir liels")
-else:
- print("Skaitlis ir mazs")
+#k1=int(input("Ievadiet skaitli: "))
+# if sk1>10:
+# print("Skaitlis ir liels")
+# else:
+#print("Skaitlis ir mazs")
+
+ # from math import *
+ # a=int(input("Ievadiet a: "))
+# b=int(input("Ievadiet b: "))
+# c=int(input("Ievadiet c: "))
+# D-b**2-4*a*c
+ # if D<0:
+#   print("Diskriminants = . Saknu nav! ")
+# elif D!=0: 
+#   x=-b/2*a 
+#   print("Ir viena sakne =", x)
+# else:
+#   x1=(-b+sgrt(D))/2*a
+#   x2=(-b-sgrt(D))/2*a
+#   print("Ir divas saknes, x1 =", x1," x2 =", x2)
+
+# from math import *
+# sk1=int(input ("Ievadiet pirmo skaitli: "))
+# sk2=int(input ("Ievadiet otro skaitli: "))
+# oper=input("Ievadiet matemātisko operāciju: ")
+# if oper=="+":
+#   print("Summa = ", sk1+sk2)
+# elif oper=="-": 
+#   print("Atņemšanas rezultāts =", sk1-sk2)
+# elif oper=="*":
+#   print("Reizinäjums ", sk1*sk2)
+# # # elif oper=="/":
+#   print("Dalijums =", sk1/sk2)
+# elif oper=="s":
+#   print("Kvadrātsakne =", sqrt(sk1))
+#else:
+#   print("Sāda matemātiskä operācija netiek atbalstita!!!")
+
+# # x_cor = int(input("В ведите ваш X: "))
+# # y_cor = int(input("В ведите ваш Y: "))
+# # if x_cor > 0 and y_cor > 0:
+# #   print(f"({x_cor};{y_cor})")
+# # elif x_cor < 0 and y_cor < 0:
+# #   print(f"({x_cor};{y_cor})")
+# # elif x_cor < 0 and y_cor == 0:
+# #   print(f"точка находится плоскасте x")
+# # elif x_cor > 0 and y_cor == 0:
+# #   print(f"точка находится плоскасте x")
+# # elif x_cor == 0 and y_cor < 0:
+# #   print("точка находится плоскасте y")
+# # elif x_cor == 0 and y_cor > 0:
+# #   print("точка находится плоскасте y")
+# # else:
+# #   print("точка на нулевой координате")
+
+# number = int(input("Ваше число: "))
+# if number > 0 and number < 10 :
+#   print("ваше число дисетичное, положительное.")
+# if number > 10 and number < 100 :
+#   print("ваше число двух значное, положительное.")
+# if number > 100 and number < 1000 :
+#   print("ваше число 3 - ох значное, положительное.")
+# if number > 1000:
+#   print("ваше число больше 3-ох значнорго, положительное.")
+# if number < 0:
+#   print("ваше число отрицательное.")
+
+while True:
+  #   corner1 = int(input("Введите первый угол: "))
+  #   corner2 = int(input("Введите второй угол: "))
+  #   corner3 = int(input("Введите третий угол: "))
+  #   if corner1 + corner2 + corner3 < 180 or corner1 + corner2 + corner3 > 180: 
+ #     print("У треугольника сумма углов должна быть 180 градусов")
+  #     continue
+  #   if corner1 == 90 or corner2 == 90 or corner3 == 
+ #     print("Прямоугольный треугольник")
+  #     break
+  #   elif corner1 < 90 and corner2 < 90 and corner3 < 90:
+  #     print("Остроугольный треугольник")
+  #     break
+  #   elif corner1 > 90 or corner2 > 90 or corner3 > 90:
+  #     print("Тупоугольный треугольник")
+
+  #num = input("Введите номер месяца: ")
+# months = {
+  #     "1": "Январь",
+  #     "2": "Февраль",
+  #     "3": "Март",
+  #     "4": "Апрель",
+  #     "5": "Май",
+  #     "6": "Июнь",
+  #     "7": "Июль",
+  #     "8": "Август",
+  #     "9": "Сентябрь",
+  #     "10": "Октябрь",
+  #     "11": "Ноябрь",
+  #     "12": "Декабрь"
+# }
+# print(f"Это {months[num]}")
+# x = int(input("Введите координату x: "))
+# y = int(input("Введите координату y: "))
+# if x == 0 and y == 0:
+  #   print("Точка находится в центре")
+# elif x == 0:
+  #   print("Точка находится на оси y")
+# elif y == 0:
+  #   print("Точка находится на оси x")
+# elif x > 0 and y > 0:
+  #   print("Точка находится в 1 квадранте")
+# elif x < 0 and y > 0:
+  #   print("Точка находится в 2 квадранте")
+# elif x < 0 and y < 0:
+  #   print("Точка находится в 3 квадранте")
+# elif x > 0 and y < 0:
+  #   print("Точка находится в 4 квадранте")
+# number = int(input("Введите число: "))
+# if number < 0:
+  #   print("Число отрицательное")
+# elif number > 0:
+  #   print("Число положительное")
+# if number < 10 and number > -10:
+  #   print("Число однозначное")
+# elif number < 100 and number > -100:
+  #   print("Число двухзначное")
+# elif number < 1000 and number > -1000:
+  #   print("Число трёхзначное")
+# elif number >= 1000 and number <= -1000:
+  #   print("Число больше трёхзначного")
