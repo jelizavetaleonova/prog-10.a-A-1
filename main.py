ade585dd77bb962d841a4d43dc73e8abf3b438bc
jelizavetal <115002331+jelizavetaleonova@users.noreply.github.com>
2022-10-11 12:03:59 +0000

    1 uzd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# sk1=int(input("Ievadiet skaitli: "))
-# if sk1>10:
-#  print("Skaitlis ir liels")
-# else:
-#  print("Skaitlis ir mazs")
+sk1=int(input("Ievadiet skaitli: "))
+if sk1>10:
+ print("Skaitlis ir liels")
+else:
+ print("Skaitlis ir mazs")
